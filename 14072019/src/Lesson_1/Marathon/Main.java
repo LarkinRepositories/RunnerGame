43b5887778f949185e5ddd5c6807d17d8ee39ad4
nineserpents <43865893+nineserpents@users.noreply.github.com>
2019-07-15 13:05:12 +0700

    добавлен this в вызове метода info() для обращения к текущему экземпляру Team

--- 14072019/src/Lesson_1/Marathon/Main.java
+++ 14072019/src/Lesson_1/Marathon/Main.java
@@ -14,6 +14,6 @@ public class Main {
         Course course = new Course(new Cross(80), new Wall(2), new Water(50), new Cross(120));
         Team teamA = new Team("Команда А", new Human("Боб"), new Cat("Барсик"), new Dog("Бобик"), new Human("Джек"));
         course.doIt(teamA);
-        teamA.showResults();
+        teamA.showResults(); //команда не справится, пока course содержит Water.
     }
 }
