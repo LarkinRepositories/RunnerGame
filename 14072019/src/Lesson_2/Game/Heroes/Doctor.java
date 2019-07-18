26418ef21ee764dd78a2897b8f94244b449fd57f
nineserpents <43865893+nineserpents@users.noreply.github.com>
2019-07-18 17:34:30 +0700

    Добавлен вывод информационных сообщений в методе healing Исправлен вывод информации

--- 14072019/src/Lesson_2/Game/Heroes/Doctor.java
+++ 14072019/src/Lesson_2/Game/Heroes/Doctor.java
@@ -1,6 +1,6 @@
 package Lesson_2.Game.Heroes;
 
-public class Doctor extends Hero implements CanHeal {
+public class Doctor extends Hero {
     private int addHeal;
     public Doctor(int heal, String name, int addHeal) {
         super(heal, name);
@@ -9,11 +9,22 @@ public class Doctor extends Hero implements CanHeal {
 
     @Override
     public void healing(Hero hero) {
-        hero.addHealth(addHeal);
+        if (hero.getHealth() <= 0) {
+            System.out.println("Не могу лечить" + hero.getName()+"!"+hero.getName()+" мертв!");
+        }
+        else {
+            hero.addHealth(addHeal);
+            System.out.println(this.getName()+" вылечил "+hero.getName()+" "+addHeal+" здоровья");
+        }
     }
 
     @Override
-    void info() {
-        System.out.println(name + " " + (health < 0 ? "Герой мертв" : health +" "+addHeal));
+    public void info() {
+        System.out.println(this.getName() + " " + (this.getHealth() < 0 ? this.getName()+  " мертв!" : this.getHealth() +" "+addHeal));
+    }
+
+    @Override
+    public void hit(Hero hero) {
+        System.out.println("Доктор не может наносить урон");
     }
 }
