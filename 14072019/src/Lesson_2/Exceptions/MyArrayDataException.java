ede0e4b188003db06fd4b65994c60cf4b225c78b
nineserpents <43865893+nineserpents@users.noreply.github.com>
2019-07-18 14:51:16 +0700

    Добавил пробел в super()

--- 14072019/src/Lesson_2/Exceptions/MyArrayDataException.java
+++ 14072019/src/Lesson_2/Exceptions/MyArrayDataException.java
@@ -3,7 +3,7 @@ package Lesson_2.Exceptions;
 public class MyArrayDataException extends Exception {
 
     public MyArrayDataException(String message, int i, int j) {
-        super(message+"["+i+"]"+"["+j+"]");
+        super(message+" ["+i+"]"+"["+j+"]");
 
     }
 }
