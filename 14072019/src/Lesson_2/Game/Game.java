b0ee0756fc583aa7793766f79d440651322d111c
nineserpents <43865893+nineserpents@users.noreply.github.com>
2019-07-19 20:49:22 +0700

    Реализованы методы teamFight и fight таким образом, чтобы команды бились, пока в 1-ой из них есть герои.

--- 14072019/src/Lesson_2/Game/Game.java
+++ 14072019/src/Lesson_2/Game/Game.java
@@ -11,7 +11,7 @@ public class Game {
         //Random randomHealing = new Random();
         //int round = 3;
 
-        Hero[] team1 = new Hero[]{new Warrior(9000, "Воин1", 700)
+        Hero[] team1 = new Hero[]{new Warrior(200, "Воин1", 700)
                 , new Assasin(150, "Ассасин1", 70)
                 , new Doctor(120, "Доктор1", 60)};
 
@@ -24,49 +24,41 @@ public class Game {
         teamFight(team1, team2);
 
     }
-    private static void teamFight(Hero[] team1, Hero[] team2) {
-        ArrayList<Hero> teamOne = new ArrayList<>(team1.length);
-        ArrayList<Hero> teamTwo = new ArrayList<>(team2.length);
-        teamOne.addAll(Arrays.asList(team1));
-        teamTwo.addAll(Arrays.asList(team2));
 
-       for (
-               int x = (int) (Math.random() * teamOne.size()-1),
-               y = (int) (Math.random() * teamTwo.size()-1);
-               x != -1 && teamOne.size() > 0 || y != -1 && teamTwo.size() > 0;
-               x = (int) (Math.random() * teamOne.size()-1), y = (int) (Math.random() * teamTwo.size()-1)
-       ) {
-                    if (teamOne.get(x) instanceof Doctor) { //TODO: добавить проверку на полное здоровье в healing();
-                        teamOne.get(x).healing(teamOne.get((int) (Math.random() * teamOne.size()-1)));
-                    } else {
-                        teamOne.get(x).hit(teamTwo.get(y));
-                        if (teamTwo.get(y).getHealth() <= 0) {
-                            System.out.println(teamTwo.get(y).getName()+" мертв!");
-                            teamTwo.remove(teamTwo.get(y));
-                            teamTwo.trimToSize();
-                            System.out.println("Теперь игроков в команде2: "+teamTwo.size());
-                        }
-                    }
-                    if (teamTwo.get(y) instanceof Doctor) {
-                        teamTwo.get(y).healing(teamTwo.get((int) (Math.random() * teamTwo.size()-1)));
-                    } else {
-                        teamTwo.get(y).hit(teamOne.get(x));
-                        if (teamOne.get(x).getHealth() <= 0) {
-                            System.out.println(teamOne.get(x).getName() + " мертв!");
-                            teamOne.remove(teamOne.get(x));
-                            teamOne.trimToSize();
-                            System.out.println("Теперь игроков в команде1: "+teamOne.size());
-                        }
-                    }
-       }
+    private static void teamFight(Hero[] team1, Hero[] team2) {
+        ArrayList<Hero> teamOne = new ArrayList<>(Arrays.asList(team1));
+        ArrayList<Hero> teamTwo = new ArrayList<>(Arrays.asList(team2));
+
+        while (teamOne.size() > 0 && teamTwo.size() > 0) {
+            fight(teamOne, teamTwo);
+            fight(teamTwo, teamOne);
+        }
         System.out.println("---------------");
 
-        for (Hero t1: teamOne) {
+        for (Hero t1 : teamOne) {
             t1.info();
         }
 
-        for (Hero t2: teamTwo) {
+        for (Hero t2 : teamTwo) {
             t2.info();
         }
     }
-}
+        private static void fight(ArrayList<Hero> teamOne, ArrayList<Hero> teamTwo) {
+            int x = (int) (Math.random() * teamOne.size());
+            int y = (int) (Math.random() * teamTwo.size());
+            if (x >= teamOne.size() || y >= teamTwo.size()) {
+            } else {
+                if (teamOne.get(x) instanceof Doctor) { //TODO: добавить проверку на полное здоровье в healing();
+                    teamOne.get(x).healing(teamOne.get((int) (Math.random() * teamOne.size() - 1)));
+                } else {
+                    teamOne.get(x).hit(teamTwo.get(y));
+                    if (teamTwo.get(y).getHealth() <= 0) {
+                        System.out.println(teamTwo.get(y).getName() + " мертв!");
+                        teamTwo.remove(teamTwo.get(y));
+                        System.out.println("Теперь игроков в команде2: " + teamTwo.size());
+                    }
+                }
+            }
+
+        }
+    }
