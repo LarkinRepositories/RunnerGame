c2cfd551248c2c7fe623d594390c2fcdef5f445c
nineserpents <43865893+nineserpents@users.noreply.github.com>
2019-07-19 18:59:09 +0700

    начат метод teamFight TODO: исправить IndexOutOfBoundsException

--- 14072019/src/Lesson_2/Game/Game.java
+++ 14072019/src/Lesson_2/Game/Game.java
@@ -1,16 +1,17 @@
 package Lesson_2.Game;
 import Lesson_2.Game.Heroes.*;
 
-import java.util.Random;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Game {
     public static void main(String[] args) {
 
-        Random randomStep = new Random();
-        Random randomHealing = new Random();
-        int round = 3;
+        //Random randomStep = new Random();
+        //Random randomHealing = new Random();
+        //int round = 3;
 
-        Hero[] team1 = new Hero[]{new Warrior(9000, "Воин1", 500)
+        Hero[] team1 = new Hero[]{new Warrior(9000, "Воин1", 700)
                 , new Assasin(150, "Ассасин1", 70)
                 , new Doctor(120, "Доктор1", 60)};
 
@@ -20,41 +21,60 @@ public class Game {
                 , new Doctor(110, "Доктор2", 80)};
 
 
+        teamFight(team1, team2);
 
-        //for (int j = 0; j < aliveMembers(team1); j++) {
-        while(!aliveMembers(team2)) {
-            for (int i = 0; i < team1.length; i++) {
-                if(randomStep.nextInt(2) == 0 && team1[i].getHealth() > 0) {
-                    if(team1[i] instanceof Doctor) {
-                        team1[i].healing(team1[randomHealing.nextInt(2)]);
-                    } else {
-                        team1[i].hit(team2[i]);
-                    }
-                } else if (team2[i].getHealth() > 0)  {
-                    if(team2[i] instanceof Doctor) {
-                        team2[i].healing(team2[randomHealing.nextInt(2)]);
-                    } else {
-                        team2[i].hit(team1[i]);
-                    }
-                }
-            }
-        }
+    }
+    private static void teamFight(Hero[] team1, Hero[] team2) {
+        ArrayList<Hero> teamOne = new ArrayList<>(team1.length);
+        ArrayList<Hero> teamTwo = new ArrayList<>(team2.length);
+        teamOne.addAll(Arrays.asList(team1));
+        teamTwo.addAll(Arrays.asList(team2));
 
+       for (
+               int x = (int) (Math.random() * teamOne.size()-1),
+               y = (int) (Math.random() * teamTwo.size()-1);
+               x != -1 && teamOne.size() > 0 || y != -1 && teamTwo.size() > 0;
+               x = (int) (Math.random() * teamOne.size()-1), y = (int) (Math.random() * teamTwo.size()-1)
+       ) {
+                    if (teamOne.get(x) instanceof Doctor) { //TODO: добавить проверку на полное здоровье в healing();
+                        teamOne.get(x).healing(teamOne.get((int) (Math.random() * teamOne.size()-1)));
+                    } else {
+                        teamOne.get(x).hit(teamTwo.get(y));
+                        if (teamTwo.get(y).getHealth() <= 0) {
+                            System.out.println(teamTwo.get(y).getName()+" мертв!");
+                            teamTwo.remove(teamTwo.get(y));
+                            teamTwo.trimToSize();
+                            System.out.println("Теперь игроков в команде2: "+teamTwo.size());
+                            if (teamOne.size() == 0) {
+                                System.out.println("Победила команда 2");
+                                break;
+                            }
+                        }
+                    }
+                    if (teamTwo.get(y) instanceof Doctor) {
+                        teamTwo.get(y).healing(teamTwo.get((int) (Math.random() * teamTwo.size()-1)));
+                    } else {
+                        teamTwo.get(y).hit(teamOne.get(x));
+                        if (teamOne.get(x).getHealth() <= 0) {
+                            System.out.println(teamOne.get(x).getName() + " мертв!");
+                            teamOne.remove(teamOne.get(x));
+                            teamOne.trimToSize();
+                            System.out.println("Теперь игроков в команде1: "+teamOne.size());
+                            if (teamTwo.size() == 0) {
+                                System.out.println("Победила команда 2");
+                                break;
+                            }
+                        }
+                    }
+       }
         System.out.println("---------------");
 
-        for (Hero t1: team1) {
+        for (Hero t1: teamOne) {
             t1.info();
         }
 
-        for (Hero t2: team2) {
+        for (Hero t2: teamTwo) {
             t2.info();
         }
-    }
-    private static boolean aliveMembers (Hero[] heroes) {
-        int aliveMember = heroes.length;
-        for(Hero hero: heroes) {
-            if (hero.getHealth() <= 0) aliveMember--;
-        }
-        return aliveMember == 0;
     }
 }
