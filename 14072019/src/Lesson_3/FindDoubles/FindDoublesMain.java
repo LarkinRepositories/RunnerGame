aa7fe5e3684004472f0f6f084cfdee3a545ba07c
nineserpents <43865893+nineserpents@users.noreply.github.com>
2019-07-22 00:34:00 +0700

    Выполнено: 1. Создать массив с набором слов (10-20 слов, среди которых должны встречаться повторяющиеся). Найти и вывести список уникальных слов, из которых состоит массив (дубликаты не считаем). Посчитать, сколько раз встречается каждое слово.

---
+++ 14072019/src/Lesson_3/FindDoubles/FindDoublesMain.java
@@ -0,0 +1,27 @@
+package Lesson_3.FindDoubles;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+
+public class FindDoublesMain {
+    public static void main(String[] args) {
+        ArrayList<String> list =  new ArrayList<>();
+        list.add("Россия");
+        list.add("Германия");
+        list.add("Россия");
+        list.add("Болгария");
+        list.add("Великобритания");
+        list.add("Германия");
+        list.add("Нидерланды");
+        list.add("Россия");
+        findDoubles(list);
+    }
+    static void findDoubles(ArrayList<String> list) {
+        HashMap<String, Integer> doubles = new HashMap<>();
+        for (String doubleValue : list) {
+                doubles.put(doubleValue, Collections.frequency(list, doubleValue));
+            }
+        System.out.println(doubles);
+    }
+}
