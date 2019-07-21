32329703515e51bccdfcd131d58dfb5a073b8e7f
nineserpents <43865893+nineserpents@users.noreply.github.com>
2019-07-21 20:20:16 +0700

    Исправлена проверка длины вложенных массивов.

--- 14072019/src/Lesson_2/Exceptions/Main.java
+++ 14072019/src/Lesson_2/Exceptions/Main.java
@@ -20,6 +20,10 @@ public class Main {
         int sum = 0;
         if (array.length !=4) throw new MyArraySizeException();
         for (int i = 0; i < array.length; i++) {
+            if (array[i].length !=4) throw new MyArraySizeException();
+        }
+
+        for (int i = 0; i < array.length; i++) {
            for (int j = 0; j < array.length; j++) {
                try {
                    sum += Integer.parseInt(array[i][j]);
