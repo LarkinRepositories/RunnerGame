f54cd6edebac407f855a48655b66ae6d8ad6cd3b
Soulandmechanism <larkinds@yandex.ru>
2019-07-22 10:15:33 +0700

    Исправлен метод add, теперь работает в соответствии с заданием: с помощью метода add() можно добавлять записи.  Следует учесть, что под одной фамилией может быть несколько телефонов, тогда при запросе такой фамилии должны выводиться все телефоны.

--- 14072019/src/Lesson_3/PhoneBook/PhoneBook.java
+++ 14072019/src/Lesson_3/PhoneBook/PhoneBook.java
@@ -3,23 +3,26 @@ package Lesson_3.PhoneBook;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+
 public class PhoneBook {
     private HashMap<String, ArrayList<String>> phoneBook;
     public PhoneBook() {
         this.phoneBook = new HashMap<>();
+        this.phoneBook.put("Фамилия3", new ArrayList<String>());
     }
 
     public void add(String surname, String phoneNumber) {
-            ArrayList<String> phoneNumbers = new ArrayList<>();
-            phoneNumbers.add(phoneNumber);
-            phoneBook.put(surname,phoneNumbers);
-            if (phoneBook.containsKey(surname))
-            phoneNumbers.add(phoneNumber);
+        ArrayList<String> phoneNumberList = this.phoneBook.get(surname);
+        if (phoneNumberList == null) {
+            phoneNumberList = new ArrayList<>();
+            phoneBook.put(surname, phoneNumberList);
+        }
+        phoneNumberList.add(phoneNumber);
     }
 
-    public void get(String keyValue) {
-        if (phoneBook.containsKey(keyValue))
-            System.out.println("Номера телефонов для фамилии "+keyValue+" "+phoneBook.get(keyValue));
+    public void get(String surname) {
+        if (phoneBook.containsKey(surname))
+            System.out.println("Номера телефонов для фамилии "+surname+" "+phoneBook.get(surname));
 
         else
             System.out.println("В справочнике нет такой фамилии.");
