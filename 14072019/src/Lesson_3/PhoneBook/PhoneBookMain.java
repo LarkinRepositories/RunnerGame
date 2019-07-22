bae4d3ee8aa23cc3393e79288f1ead7e79c80021
Soulandmechanism <larkinds@yandex.ru>
2019-07-22 10:16:16 +0700

    Выполнено домашнее заданее: Написать простой класс ТелефонныйСправочник, который хранит в себе список фамилий и телефонных номеров. В этот телефонный справочник с помощью метода add() можно добавлять записи. С помощью метода get() искать номер телефона по фамилии. Следует учесть, что под одной фамилией может быть несколько телефонов, тогда при запросе такой фамилии должны выводиться все телефоны.

--- 14072019/src/Lesson_3/PhoneBook/PhoneBookMain.java
+++ 14072019/src/Lesson_3/PhoneBook/PhoneBookMain.java
@@ -3,8 +3,10 @@ package Lesson_3.PhoneBook;
 public class PhoneBookMain {
     public static void main(String[] args) {
         PhoneBook phoneBook = new PhoneBook();
-        phoneBook.add("Фамилия3","+798919919919");
-        phoneBook.add("Фамилия3","+798919919719");
+        phoneBook.add("Фамилия2","+798919919919");
+        phoneBook.add("Фамилия2","+798919919719");
+        phoneBook.add("Фамилия3", "+7193181919919");
+        phoneBook.get("Фамилия2");
         phoneBook.get("Фамилия3");
     }
 }
