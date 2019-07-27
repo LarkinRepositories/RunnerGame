5fcd16a17b4e79754b5b6e16fcb5ac85e43b1858
nineserpents <43865893+nineserpents@users.noreply.github.com>
2019-07-27 11:12:45 +0700

    first commit

---
+++ СhatWindow/src/sample/Controller.java
@@ -0,0 +1,45 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    @FXML
+    private AnchorPane rootPane;
+    @FXML
+    private TextArea inputMessageArea;
+    @FXML
+    private TextFlow emojiList;
+    @FXML
+    private Button btnEmoji;
+
+    @FXML
+    void emojiAction(ActionEvent event) {
+        if(emojiList.isVisible()){
+
+            emojiList.setVisible(false);
+        }else {
+            emojiList.setVisible(true);
+        }
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        for (Node text: emojiList.getChildren()) {
+            text.setOnMouseClicked(event -> {
+            inputMessageArea.setText(inputMessageArea.getText()+" "+((Text)text).getText());
+            emojiList.setVisible(false);
+            });
+        }
+    }
+}
