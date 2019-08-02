7627f292de6083884ea99ea07f9f600339f6b318
Dmitriy <larkingit@yandex.ru>
2019-08-02 15:05:16 +0700

    Вынесена логика обработки событий клиента в отдельный класс

---
+++ 14072019/src/Lesson_6/Server/ClientHandler.java
@@ -0,0 +1,57 @@
+package Lesson_6.Server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class ClientHandler {
+    private Server server;
+    private Socket socket;
+    private DataOutputStream out;
+    private DataInputStream in;
+
+    public ClientHandler(Server server, Socket socket) {
+        try {
+            this.socket = socket;
+            this.server = server;
+            this.in = new DataInputStream(socket.getInputStream());
+            this.out = new DataOutputStream(socket.getOutputStream());
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        while (true) {
+                            String message = in.readUTF();
+                            System.out.println("Client: " + message);
+                            if (message.equals("/end")) {
+                                break;
+                            }
+                           server.broadcastMessage(message);
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } finally {
+                        try {
+                            in.close();
+                            out.close();
+                            socket.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }).start();
+            } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+    public void sendMessage(String message) {
+        try {
+            out.writeUTF(message);
+            out.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
