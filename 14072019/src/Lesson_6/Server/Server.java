8809f5bfac80fef951d4739fa0b6791393c5ccf7
Soulandmechanism <larkinds@yandex.ru>
2019-08-02 10:41:04 +0700

    Написан эхо-сервер

--- 14072019/src/Lesson_6/Server/Server.java
+++ 14072019/src/Lesson_6/Server/Server.java
@@ -1,8 +1,6 @@
 package Lesson_6.Server;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -17,9 +15,12 @@ public class Server {
             socket = server.accept();
             System.out.println("Client connected!");
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter out = new PrintWriter(socket.getOutputStream());
             while (true) {
                 String str = in.readLine();
                 System.out.println("Client: " +str);
+                out.println("Echo: " +str);
+                out.flush();
             }
         } catch (IOException e) {
             e.printStackTrace();
