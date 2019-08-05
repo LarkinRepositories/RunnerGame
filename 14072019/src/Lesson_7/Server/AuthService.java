7191309d6da4c0577ca8e7dcb7cf657c2029d1cf
Dmitriy <larkingit@yandex.ru>
2019-08-05 14:20:49 +0700

    добавлен класс AuthService для соединения с базой

---
+++ 14072019/src/Lesson_7/Server/AuthService.java
@@ -0,0 +1,28 @@
+package Lesson_7.Server;
+
+import java.sql.*;
+
+public class AuthService {
+    private static Connection connection;
+    private static Statement statement;
+
+    public static void connect() throws ClassNotFoundException, SQLException {
+        Class.forName("org.postgresql.Driver");
+        connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/users");
+    }
+
+    public static void disconnect() {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String getNickNameByLoginAndPass(String login, String password) throws SQLException {
+        String query = String.format("SELECT nickname FROM users WHERE login='%s' and password = '%s'", login, password);
+        ResultSet rs = statement.executeQuery(query);
+        if (rs.next()) return rs.getString(1);
+        return null;
+    }
+}
